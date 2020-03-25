e5b50028750f9ffddc5b2101855c0541e9536433
renanslivka <renanslivka@gmail.com>
2020-03-25 11:13:12 +0200

    adding header component and link to home via logo

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import HomePage from "./pages/homepage/homepage";
 import { Switch, Route } from "react-router-dom";
 import ShopPage from "./pages/shop/shop";
+import Header from "./components/header/header";
 
 const HatsPage = () => {
   return <h1>HATS PAGE</h1>;
@@ -11,6 +12,7 @@ const HatsPage = () => {
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
