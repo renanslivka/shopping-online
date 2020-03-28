c1df05932af4520cee6819e71539d5f6337c62e5
renanslivka <renanslivka@gmail.com>
2020-03-28 17:00:43 +0300

    cart redux files now handle adding an item, also connected it to updated collection-item.

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,6 +1,7 @@
 import CartActionTypes from "./cart.types";
 const INITIAL_STATE = {
-  hidden: true
+  hidden: true,
+  cartItems: []
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
@@ -10,7 +11,11 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         ...state,
         hidden: !state.hidden
       };
-
+    case CartActionTypes.ADD_ITEMS:
+      return {
+        ...state,
+        cartItems: [...state.cartItems, action.payload]
+      };
     default:
       return state;
   }
