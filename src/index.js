06ed5ab06961dfbf231646d2568c24c1d1009451
renanslivka <renanslivka@gmail.com>
2020-03-29 19:24:38 +0300

    added redux-persist, now if users do refresh to the checkout page the users don't lose their cart

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import "./index.css";
 import App from "./App";
 import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./redux/store";
 
 ReactDOM.render(
   <Provider store={store}>
     <HashRouter>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </HashRouter>
   </Provider>,
   document.getElementById("root")
