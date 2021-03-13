1581917deab60725202988346e36d7bfc7bdbaa8
renan <renanslivka@gmail.com>
2021-03-13 02:18:18 +0200

    add gmail api to server and fix bug that email dont send in contact page

--- server/send-email.js
+++ server/send-email.js
@@ -1,20 +1,31 @@
 const nodemailer = require("nodemailer");
+const { google } = require('googleapis');
 require("dotenv").config();
+
+const oAuth2Client = new google.auth.OAuth2(
+  process.env.CLIENT_ID,
+  process.env.CLEINT_SECRET,
+  process.env.REDIRECT_URI
+);
+oAuth2Client.setCredentials({ refresh_token: process.env.REFRESH_TOKEN });
 
 async function sendEmail(req, res) {
   try {
-    var transport = {
+    const accessToken = await oAuth2Client.getAccessToken();
+    const transport = {
       host: "smtp.gmail.com",
       port: 587,
-      secure: false,
       auth: {
         user: process.env.USER,
-        pass: process.env.PASS,
+        type: 'OAuth2',
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLEINT_SECRET,
+        refreshToken: process.env.REFRESH_TOKEN,
+        accessToken: accessToken,
       },
     };
-
-    var transporter = nodemailer.createTransport(transport);
-
+    const transporter = nodemailer.createTransport(transport);
+    
     transporter.verify((error, success) => {
       if (error) {
         console.log(error);
@@ -22,12 +33,12 @@ async function sendEmail(req, res) {
         console.log("Server is ready to take messages");
       }
     });
-    var name = req.body.name;
-    var email = req.body.email;
-    var message = req.body.message;
-    var content = `name: ${name} \n email: ${email} \n message: ${message} `;
+    const name = req.body.name;
+    const email = req.body.email;
+    const message = req.body.message;
+    const content = `name: ${name} \n email: ${email} \n message: ${message} `;
 
-    var mail = {
+    const mail = {
       from: email,
       to: process.env.USER, // Change to email address that you want to receive messages on
       subject: "New Message from " + name,
@@ -46,7 +57,7 @@ async function sendEmail(req, res) {
       }
     });
   } catch (error) {
-    res.status(500).json({
+    res.status(error.code).json({
       message: error.message,
     });
   }
